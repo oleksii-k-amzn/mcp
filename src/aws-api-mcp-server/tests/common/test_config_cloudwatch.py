1da6b7b416b754974ed5e0fc9f51091440a89097
Roman Shevchuk <rshev@amazon.com>
2025-09-01 13:43:17 +0200

    Change to EXPERIMENTAL_ feature

--- src/aws-api-mcp-server/tests/common/test_config_cloudwatch.py
+++ src/aws-api-mcp-server/tests/common/test_config_cloudwatch.py
@@ -5,7 +5,7 @@ from unittest.mock import patch
 
 
 def test_cloudwatch_log_group_name_not_set():
-    """Test when AWS_API_MCP_CLOUDWATCH_LOG_GROUP is not set."""
+    """Test when EXPERIMENTAL_AWS_API_MCP_LOG_GROUP is not set."""
     with patch.dict(os.environ, {}, clear=True):
         # Reload the config module to get fresh environment variables
         import awslabs.aws_api_mcp_server.core.common.config as config_module
@@ -17,10 +17,12 @@ def test_cloudwatch_log_group_name_not_set():
 
 
 def test_cloudwatch_log_group_name_set():
-    """Test when AWS_API_MCP_CLOUDWATCH_LOG_GROUP is set."""
+    """Test when EXPERIMENTAL_AWS_API_MCP_LOG_GROUP is set."""
     test_log_group = '/aws/mcp/aws-api-mcp-server'
 
-    with patch.dict(os.environ, {'AWS_API_MCP_CLOUDWATCH_LOG_GROUP': test_log_group}, clear=True):
+    with patch.dict(
+        os.environ, {'EXPERIMENTAL_AWS_API_MCP_LOG_GROUP': test_log_group}, clear=True
+    ):
         # Reload the config module to get fresh environment variables
         import awslabs.aws_api_mcp_server.core.common.config as config_module
         import importlib
@@ -31,8 +33,8 @@ def test_cloudwatch_log_group_name_set():
 
 
 def test_cloudwatch_log_group_name_empty_string():
-    """Test when AWS_API_MCP_CLOUDWATCH_LOG_GROUP is set to empty string."""
-    with patch.dict(os.environ, {'AWS_API_MCP_CLOUDWATCH_LOG_GROUP': ''}, clear=True):
+    """Test when EXPERIMENTAL_AWS_API_MCP_LOG_GROUP is set to empty string."""
+    with patch.dict(os.environ, {'EXPERIMENTAL_AWS_API_MCP_LOG_GROUP': ''}, clear=True):
         # Reload the config module to get fresh environment variables
         import awslabs.aws_api_mcp_server.core.common.config as config_module
         import importlib
@@ -43,10 +45,12 @@ def test_cloudwatch_log_group_name_empty_string():
 
 
 def test_cloudwatch_log_group_name_special_characters():
-    """Test when AWS_API_MCP_CLOUDWATCH_LOG_GROUP contains special characters."""
+    """Test when EXPERIMENTAL_AWS_API_MCP_LOG_GROUP contains special characters."""
     test_log_group = '/aws/mcp/test-group-with-dashes_and_underscores'
 
-    with patch.dict(os.environ, {'AWS_API_MCP_CLOUDWATCH_LOG_GROUP': test_log_group}, clear=True):
+    with patch.dict(
+        os.environ, {'EXPERIMENTAL_AWS_API_MCP_LOG_GROUP': test_log_group}, clear=True
+    ):
         # Reload the config module to get fresh environment variables
         import awslabs.aws_api_mcp_server.core.common.config as config_module
         import importlib
@@ -57,10 +61,12 @@ def test_cloudwatch_log_group_name_special_characters():
 
 
 def test_cloudwatch_log_group_name_very_long():
-    """Test when AWS_API_MCP_CLOUDWATCH_LOG_GROUP is very long."""
+    """Test when EXPERIMENTAL_AWS_API_MCP_LOG_GROUP is very long."""
     test_log_group = '/aws/mcp/' + 'a' * 200  # Very long log group name
 
-    with patch.dict(os.environ, {'AWS_API_MCP_CLOUDWATCH_LOG_GROUP': test_log_group}, clear=True):
+    with patch.dict(
+        os.environ, {'EXPERIMENTAL_AWS_API_MCP_LOG_GROUP': test_log_group}, clear=True
+    ):
         # Reload the config module to get fresh environment variables
         import awslabs.aws_api_mcp_server.core.common.config as config_module
         import importlib
@@ -71,10 +77,12 @@ def test_cloudwatch_log_group_name_very_long():
 
 
 def test_cloudwatch_log_group_name_with_unicode():
-    """Test when AWS_API_MCP_CLOUDWATCH_LOG_GROUP contains unicode characters."""
+    """Test when EXPERIMENTAL_AWS_API_MCP_LOG_GROUP contains unicode characters."""
     test_log_group = '/aws/mcp/test-group-🚀-with-emoji'
 
-    with patch.dict(os.environ, {'AWS_API_MCP_CLOUDWATCH_LOG_GROUP': test_log_group}, clear=True):
+    with patch.dict(
+        os.environ, {'EXPERIMENTAL_AWS_API_MCP_LOG_GROUP': test_log_group}, clear=True
+    ):
         # Reload the config module to get fresh environment variables
         import awslabs.aws_api_mcp_server.core.common.config as config_module
         import importlib
@@ -85,10 +93,12 @@ def test_cloudwatch_log_group_name_with_unicode():
 
 
 def test_cloudwatch_log_group_name_case_sensitive():
-    """Test that AWS_API_MCP_CLOUDWATCH_LOG_GROUP is case sensitive."""
+    """Test that EXPERIMENTAL_AWS_API_MCP_LOG_GROUP is case sensitive."""
     test_log_group = '/AWS/MCP/TEST-GROUP'
 
-    with patch.dict(os.environ, {'AWS_API_MCP_CLOUDWATCH_LOG_GROUP': test_log_group}, clear=True):
+    with patch.dict(
+        os.environ, {'EXPERIMENTAL_AWS_API_MCP_LOG_GROUP': test_log_group}, clear=True
+    ):
         # Reload the config module to get fresh environment variables
         import awslabs.aws_api_mcp_server.core.common.config as config_module
         import importlib
@@ -99,10 +109,12 @@ def test_cloudwatch_log_group_name_case_sensitive():
 
 
 def test_cloudwatch_log_group_name_with_numbers():
-    """Test when AWS_API_MCP_CLOUDWATCH_LOG_GROUP contains numbers."""
+    """Test when EXPERIMENTAL_AWS_API_MCP_LOG_GROUP contains numbers."""
     test_log_group = '/aws/mcp/test-group-123'
 
-    with patch.dict(os.environ, {'AWS_API_MCP_CLOUDWATCH_LOG_GROUP': test_log_group}, clear=True):
+    with patch.dict(
+        os.environ, {'EXPERIMENTAL_AWS_API_MCP_LOG_GROUP': test_log_group}, clear=True
+    ):
         # Reload the config module to get fresh environment variables
         import awslabs.aws_api_mcp_server.core.common.config as config_module
         import importlib
@@ -113,10 +125,12 @@ def test_cloudwatch_log_group_name_with_numbers():
 
 
 def test_cloudwatch_log_group_name_multiple_slashes():
-    """Test when AWS_API_MCP_CLOUDWATCH_LOG_GROUP contains multiple slashes."""
+    """Test when EXPERIMENTAL_AWS_API_MCP_LOG_GROUP contains multiple slashes."""
     test_log_group = '/aws/mcp/deeply/nested/log/group'
 
-    with patch.dict(os.environ, {'AWS_API_MCP_CLOUDWATCH_LOG_GROUP': test_log_group}, clear=True):
+    with patch.dict(
+        os.environ, {'EXPERIMENTAL_AWS_API_MCP_LOG_GROUP': test_log_group}, clear=True
+    ):
         # Reload the config module to get fresh environment variables
         import awslabs.aws_api_mcp_server.core.common.config as config_module
         import importlib
